40579286517b68b06ae09837b0ef1ad37a643286
arjunjnair01 <arjunjnair01@gmail.com>
2026-09-16 21:37:05 +0530

    Defining Controllers

---
+++ TransactionMicroservice/src/main/java/com/oracle/transactionmicroservice/controllers/WalletController.java
@@ -0,0 +1,9 @@
+package com.oracle.transactionmicroservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/wallets")
+public class WalletController {
+}
