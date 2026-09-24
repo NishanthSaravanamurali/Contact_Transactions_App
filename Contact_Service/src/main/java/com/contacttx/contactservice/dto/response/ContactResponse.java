9ad17ec1b105c045fa2604e412da9fed794c6930
Mugunth Narayanan <mugunthnarayanan@gmail.com>
2026-09-24 11:58:32 +0530

    conflict issue resolved in integration branch

--- Contact_Service/src/main/java/com/contacttx/contactservice/dto/response/ContactResponse.java
+++ Contact_Service/src/main/java/com/contacttx/contactservice/dto/response/ContactResponse.java
@@ -71,14 +71,6 @@ public class ContactResponse {
         return linkedToRegisteredUser;
     }
 
-    public Long getLinkedUserId() {
-        return linkedUserId;
-    }
-
-    public void setLinkedUserId(Long linkedUserId) {
-        this.linkedUserId = linkedUserId;
-    }
-
     public void setLinkedToRegisteredUser(boolean linkedToRegisteredUser) {
         this.linkedToRegisteredUser = linkedToRegisteredUser;
     }
